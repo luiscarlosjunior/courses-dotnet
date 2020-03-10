9355dcb23670efe4352cc2391c2ab121d4497960
Luis Santos <luis.carlosjr@hotmail.com.br>
2020-03-09 21:53:51 -0300

    Added new projects to start some concepts on the .net
    Was added the project: thread and delegatesEvents

---
+++ UdemyCourses/UdemyCourses/PointsAndLines/Point.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyCourses
+{
+    class User
+    {
+        private string username;
+        private int password;
+
+        public string Username {
+            get
+            {
+                return "The user is " + username;
+            }            
+        }
+
+        public int Password {
+            set
+            {
+                if (value >= 4 && value <= 10)
+                {
+                    password = value;
+                }    
+                else
+                {
+                    Console.WriteLine("Opps");
+                }
+            }
+        }
+    }
+
+    class Point
+    {
+        public int x;
+        public int y;
+
+        public Point(int x, int y)
+        {
+
+        }
+    }
+}
---
+++ UdemyCourses/UdemyCourses/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyCourses
+{
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Nomal pointer to an object.
+            int[] a = new int[5] {10, 20, 30, 40, 50 };
+
+            //Must be in unsafe code to use interior pointers
+            unsafe
+            {
+                fixed (int* p = &a[0])
+                {
+                    // p is pinned as well as object, so create another
+                    int* p2 = p;
+                    Console.WriteLine(*p2);
+
+                    // Incrementing p2 bumps the pointer by foir bytes
+                    p2 += 1;
+                    Console.WriteLine(*p2);
+                    p2 += 1;
+                    Console.WriteLine(*p2);
+                    Console.WriteLine("--------");
+                    Console.WriteLine(*p);
+
+                    //Dereferencing p and incrementing changes
+                    *p += 1;
+                    Console.WriteLine(*p);
+                    *p += 1;
+                    Console.WriteLine(*p);
+                }
+            }
+
+            Console.WriteLine("-----");
+            Console.WriteLine(a[0]);
+        }
+    }
+}
