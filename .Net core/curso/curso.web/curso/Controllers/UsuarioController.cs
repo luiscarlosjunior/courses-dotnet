8208f71b94230a089af11e69374b85c76b838da4
luiscarlosjunior <luis.carlosjr@hotmail.com.br>
2020-12-21 22:10:24 -0300

    Added a project that will works with API in ASP Core

---
+++ .Net core/curso/curso.web/curso/Controllers/UsuarioController.cs
@@ -0,0 +1,18 @@
+﻿using curso.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace curso.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Cadastrar(RegistrarUsuarioViewModelInput registrarUsuarioViewModelInput)
+        {
+            return View();
+        }
+    }
+}
